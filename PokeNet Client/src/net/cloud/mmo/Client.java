d01f869eea23727777cba9f1d8b30dadb948ed27
ThatOneCloud <corn0317@hotmail.com>
2014-11-04 21:23:07 -0600

    Commit from Laptop test

--- PokeNet Client/src/net/cloud/mmo/Client.java
+++ PokeNet Client/src/net/cloud/mmo/Client.java
@@ -1,9 +1,10 @@
+package net.cloud.mmo;
 
 public class Client {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println("Changed on laptop");
 	}
 
 }
