0023f0cb18ae7f5c759b59a321af14ac1cbf9501
ThatOneCloud <corn0317@hotmail.com>
2014-12-24 21:30:05 -0600

    Initial JUnit implementation Just a small commit since JUnit was just placed in. Some cases for some utility methods are included

---
+++ PokeNet Server/test/net/cloud/mmo/AllTests.java
@@ -0,0 +1,16 @@
+package net.cloud.mmo;
+
+import net.cloud.mmo.util.UtilSuite;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+	UtilSuite.class
+})
+public class AllTests {
+	// Nothing goes here. Annotations are all we need
+}
+
---
+++ PokeNet Server/test/net/cloud/mmo/util/UtilSuite.java
@@ -0,0 +1,13 @@
+package net.cloud.mmo.util;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({
+	StringUtilTest.class
+})
+public class UtilSuite {
+
+}
